4c609602bd6b8cad6153a7252154009211f7e2bf
saud alnasser <saud4services@gmail.com>
2022-10-26 20:50:19 +0300

    add main header

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <main>
+    <header>
+      <h1>Counter App</h1>
+    </header>
+  </main>
+</template>
+
+<style scoped>
+main {
+  height: 100vh;
+  width: 100vw;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: aliceblue;
+}
+</style>
