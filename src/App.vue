a00379ab485fc2d7d30e1deee6201f827b82d8d3
saud alnasser <saud4services@gmail.com>
2022-10-26 21:24:43 +0300

    add counter

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,16 @@
 <template>
   <main>
     <header>
-      <h1>Counter App</h1>
+      <h1 id="app-header">Counter App</h1>
     </header>
+    <section>
+      <article>
+        <h2 id="counter-header">The current count is...</h2>
+        <h2 id="counter">0</h2>
+        <button id="decrement-btn">-</button>
+        <button id="increment-btn">+</button>
+      </article>
+    </section>
   </main>
 </template>
 
@@ -10,9 +18,42 @@
 main {
   height: 100vh;
   width: 100vw;
+  background: aliceblue;
   display: flex;
+  flex-direction: column;
   align-items: center;
   justify-content: center;
-  background-color: aliceblue;
+  text-align: center;
+}
+
+#app-header {
+  font-size: 3rem;
+}
+
+#counter-header {
+  font-size: 2rem;
+}
+
+#counter {
+  font-weight: bold;
+  font-size: 4rem;
+}
+
+#increment-btn,
+#decrement-btn {
+  width: 7rem;
+  height: 2.25rem;
+  border-radius: 50px;
+  font-size: 1.25rem;
+  text-align: center;
+  margin: 15px;
+}
+
+#increment-btn {
+  background-color: #a8e890;
+}
+
+#decrement-btn {
+  background-color: #dd5353;
 }
 </style>
